1c0eb33eed8215a55725d30e011de7c405778acf
Kazuto Kirigaya <102516901+KazutoKirigayaXd@users.noreply.github.com>
2024-04-09 13:56:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ API_ID = int(getenv("API_ID", ADD_HERE)) #In Integer dont add quotes
 API_HASH = getenv("API_HASH", "ADD_HERE")
 BOT_TOKEN = getenv("BOT_TOKEN", "ADD_HERE") #Put your bot token here
 CHANNEL = getenv("CHANNEL", "ADD_HERE") #Your public channel username without @ for force subscription.
+GROUP = getenv("GROUP", "ADD_HERE") #Your public channel username without @ for force subscription.
 MONGO = getenv("MONGO", "ADD_HERE") #Put mongo db url here
 #Optional Variables
 OWNER_ID = int(getenv("OWNER_ID", "ADD_HERE")) #Put Owner Id
